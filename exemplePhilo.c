200dbfaf5375cd8244211ac7acb7798b8edcca7f
raphael <raphael.laurent@etu.univ-amu.fr>
2021-12-03 10:00:24 +0100

    3/11

--- exemplePhilo.c
+++ exemplePhilo.c
@@ -7,6 +7,8 @@
 pthread_mutex_t mutex     = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  condition = PTHREAD_COND_INITIALIZER;
 
+typedef enum {RIPPAILLE, AUTRE} SITUATION;
+
 
 int maint(void){
     int n = 5;
